728948b50c24fffedd74ba765ebd0f28347caaa5
Vipin nagar <vipinnagar8700@gmail.com>
2023-12-08 09:16:47 +0530

    Cors

--- src/index.js
+++ src/index.js
@@ -17,11 +17,23 @@ dbConnect();
 // Serve static files from the 'public' directory
 app.use(express.static('public'));
 
+
+app.use(cors({
+    origin: 'https://life-care-solutionq.vercel.app/',
+    credentials: true,
+}));
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
+
+
 app.get('/', (req, res) => {
-    res.status(200).json([{ message: 'Hello Mr vipin Nagar',status:true }])
+    res.status(200).json([{ message: 'Hello Mr vipin Nagar', status: true }])
 
 })
-app.use('/api',userRoutes)
+app.use('/api', userRoutes)
 
 
 
